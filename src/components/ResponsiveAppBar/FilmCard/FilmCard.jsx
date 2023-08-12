73e7208c312fc9822bb80c8bea2356437378b248
Anastasia <kop.anastasia27@gmail.com>
2023-08-12 14:51:48 +0300

    change style of card

--- src/components/ResponsiveAppBar/FilmCard/FilmCard.jsx
+++ src/components/ResponsiveAppBar/FilmCard/FilmCard.jsx
@@ -3,7 +3,8 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { Box, CardActionArea } from '@mui/material';
+
 
 export default function FilmCard({ 
     id, 
@@ -18,20 +19,53 @@ export default function FilmCard({
 
   } 
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 395,
+      height: 222,
+      position: "relative"}}>
       <CardActionArea>
         <CardMedia
           component="img"
-          height="140"
+          height="395"
           image={image}
           alt={name}
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {name}
-          </Typography>
-          <button onClick={handleClik}>Clik me</button>
-        </CardContent>
+        <Box sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%", 
+          backgroundImage:"linear-gradient(90deg, rgba(0, 0, 0, 0.80) 0%, rgba(20, 20, 20, 0.40) 50%, rgba(83, 100, 141, 0.00) 100%)"}}>
+            <Box sx={{
+              position:"absolute",
+              justifyContent:"space-evenly",
+              alignItems:"flex-start",
+              display:"flex",
+              flexDirection:"column",
+              left:10,
+              top:100,  
+             }}>
+              <Typography gutterBottom variant="p" component="div" sx={{
+                color:"#fff", fontSize:15
+               }}>
+               {name}
+              </Typography>
+              <Typography gutterBottom variant="p" component="div" sx={{
+                color:"#fff", fontSize:12, paddingBottom:1, paddingTop:1
+               }}>
+                {time}
+              </Typography>
+              <button onClick={handleClik} style={{
+                border: "1px solid #E50914", 
+                background: "#E50914 ",
+                width: 100,
+                height: 30, 
+                color: "#fff"
+               }}>SHOW MORE</button>
+
+            </Box>
+        </Box>
+        
       </CardActionArea>
     </Card>
   );
