f816cc1f8d7bd8b56a56e37e07542fb1326421fe
Anastasia <kop.anastasia27@gmail.com>
2023-08-22 11:02:23 +0300

    film details

--- src/pages/FilmDetails.jsx
+++ src/pages/FilmDetails.jsx
@@ -1,10 +1,31 @@
+
 import { useParams } from "react-router";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 
 function FilmDetails() {
+    const [showData, setShowData] = useState([]);
     const {filmId} = useParams();
-    return (
-        <h1>This is film with ID: {filmId}</h1>
-    )
+    console.log(filmId);
+
+    useEffect(() => {
+        async function requestAboutFilm(){
+          try {
+              const response = await axios.get(`http://www.tvmaze.com/shows/${filmId}`);
+              setShowData(response.data);
+              console.log(filmId);
+              console.log(showData);
+          } catch (error) {
+            console.error(error);
+          }
+        }
+        requestAboutFilm();
+      }, [filmId]);
+
+    return showData;
+    
 }
+ 
 
 export default FilmDetails;
