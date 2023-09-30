fde40adb1cfa59482d1dd937e4e48044cdd199aa
Anastasia <kop.anastasia27@gmail.com>
2023-09-30 23:43:32 +0300

    added the ability to add or change avatar picture

--- src/pages/Auth/Profile.jsx
+++ src/pages/Auth/Profile.jsx
@@ -1,18 +1,23 @@
 import { Grid } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
 import { getAuth } from "firebase/auth";
+import {Button } from '@mui/material';
+import userAddingPictureToStorage from './userAddingPictureToStorage';
+
 
 function Profile() { 
     const auth = getAuth();
     const user = auth.currentUser;
-    console.log(user);
-    if (user !== null) {
-        const displayName = user.displayName;
-        const email = user.email;
-        const photoURL = user.photoURL;
-        const emailVerified = user.emailVerified;
+
+    const handleUploadAvatarToStorage = (e) => {
+        const file = e.target.files[0];
+        userAddingPictureToStorage(file)
     }
-    
+
+    const handleReload = () => {
+        window.location.reload()
+    }
+
 
     return (
         <Grid container 
@@ -34,6 +39,11 @@ function Profile() {
                 <h3 style={{margin:'10px'}}>{user.displayName}</h3>
                 <p style={{margin:"10px 10px 0"}}>Email</p>
                 <h5 style={{margin:'0 10px'}}>{user.email}</h5>
+                <p style={{margin:'50px 10px', fontSize:'12px'}}>Would you like to add or change your profile picture?</p>
+                <input type="file" id='avatar' name='avatar' accept='image/png, image/jpeg' onChange={handleUploadAvatarToStorage}/>
+                <Button sx={{marginTop:'5px'}} onClick={handleReload} variant="outlined" size='small'>
+                        Upload
+                </Button>
             </Grid>
         </Grid>
     )
